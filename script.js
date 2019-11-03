61f7e535bdf004c8bb1437853442370253497625
brandon cauley <cauleytyler@gmail.com>
2019-11-03 16:18:25 -0500

    fixing prompts

--- script.js
+++ script.js
@@ -6,29 +6,27 @@ var clipboard = document.getElementById("clipboard");
  length = parseInt(prompt("To ensure security, choose a character amount between 8 & 128"));{
     if(length > 7 && length < 129) {
       
-          
-         confirm("Would you like to use lowercase letters?");{
-            useLower = ("abcdefghijklmnopqrstuvwxyz");{
-            if(false.valueOfuseLower = (""));
-           
-            }}
+           var useLower 
+         if (confirm("Would you like to use lowercase letters?")==true)
+            {
+                useLower.value = "abcdefghijklmnopqrstuvwxyz";
+            } 
+            
 
          
          confirm("Would you like to use uppercase letters?");{
-            useUpper = ("ABCDEFGHIJKLMNOPQRSTUVWZYZ");{
-            if(false.valueOfuseUpper = (""));
+            if(useUpper = ("ABCDEFGHIJKLMNOPQRSTUVWZYZ")){
+                
          }}
         
           
          confirm("Would you like to use numbers?");{
-            useNumbers = ("1234567890");{
-            if(false.valueOfuseNumbers = (""));
+            if(useNumbers = ("1234567890"));{
          }}
         
           
          confirm("Would you like to use symbols?");{
-            useSymbols = ("!@#$%^&*()_+");{
-            if(false.valueOfuseSymbols = (""));
+            if(useSymbols = ("!@#$%^&*()_+"));{
          }}
             
         
@@ -50,4 +48,4 @@ var clipboard = document.getElementById("clipboard");
     console.log(useUpper);
 }
 
-
+ 
